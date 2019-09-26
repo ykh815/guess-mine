a25efb3c7666776e6578d09e8cb5763e85a52ba1
Kwnghyun Yoo <ykh815@hotmail.com>
2019-09-26 22:46:37 +0900

    Hello World

--- src/static/index.js
+++ src/static/index.js
@@ -1,1 +1,6 @@
-hello;
+// eslint-disable-next-line no-undef
+const socket = io("/");
+
+socket.on("hello", () => console.log("Somebody joined"));
+
+setTimeout(() => socket.emit("helloGuys"), 4000);
