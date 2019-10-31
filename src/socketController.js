0b5c4398f40ca953b4dc918b76ee9f12828c39cb
Kwnghyun Yoo <ykh815@hotmail.com>
2019-10-31 22:49:34 +0900

    Chat

--- src/socketController.js
+++ src/socketController.js
@@ -4,14 +4,16 @@ const socketController = socket => {
   const broadcast = (event, data) => socket.broadcast.emit(event, data);
 
   socket.on(events.setNickname, ({ nickname }) => {
-    console.log("New", nickname);
     socket.nickname = nickname;
     broadcast(events.newUser, { nickname });
   });
 
   socket.on(events.disconnect, () => {
-    console.log("Del", socket.nickname);
     broadcast(events.disconnected, { nickname: socket.nickname });
+  });
+
+  socket.on(events.sendMsg, ({ message }) => {
+    broadcast(events.newMsg, { message, nickname: socket.nickname });
   });
 };
 
