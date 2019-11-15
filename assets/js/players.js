301828719e3c3478a51ed7a10580d48056926b28
Kwanghyun Yoo <ykh815@hotmail.com>
2019-11-15 13:59:41 +0900

    Game Programming part Four

--- assets/js/players.js
+++ assets/js/players.js
@@ -5,6 +5,7 @@ import {
   showControls,
   resetCanvas
 } from "./paint";
+import { disableChat, enableChat } from "./chat";
 
 const board = document.getElementById("jsPBoard");
 const notifs = document.getElementById("jsNotifs");
@@ -32,11 +33,14 @@ export const handleGameStarted = () => {
 export const handleLeaderNotif = ({ word }) => {
   enableCanvas();
   showControls();
+  disableChat();
   setNotifs(`You are the painter, paint : ${word}`);
 };
 export const handleGameEnded = () => {
   setNotifs("Game ended");
   disableCanvas();
   hideControls();
+  enableChat();
   resetCanvas();
 };
+export const handleGameStarting = () => setNotifs("Game will start soon");
